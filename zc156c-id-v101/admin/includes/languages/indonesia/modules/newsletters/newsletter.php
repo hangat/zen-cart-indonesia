d0643701b62b15acce2fa78d92c78fbca9f41c9c
Hangat Perkasa <ihangat2000@gmail.com>
2019-07-29 22:49:32 +0700

    Add files via upload

--- zc156c-id-v101/admin/includes/languages/indonesia/modules/newsletters/newsletter.php
+++ zc156c-id-v101/admin/includes/languages/indonesia/modules/newsletters/newsletter.php
@@ -1,1 +1,36 @@
+<?php
+/**
+ * @package admin
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: newsletter.php 4385 2006-09-04 04:10:48Z drbyte $
+ */
 
+define('TEXT_COUNT_CUSTOMERS', 'Pelanggan yang menerima buletin: %s');
+define('HEADING_TITLE', 'Manajer Buletin');
+
+define('TABLE_HEADING_NEWSLETTERS', 'Buletin');
+define('TABLE_HEADING_SIZE', 'Ukuran');
+define('TABLE_HEADING_MODULE', 'Modul');
+define('TABLE_HEADING_SENT', 'Kirim');
+define('TABLE_HEADING_STATUS', 'Status');
+define('TABLE_HEADING_ACTION', 'Tindakan');
+
+define('TEXT_NEWSLETTER_MODULE', 'Modul:');
+define('TEXT_NEWSLETTER_TITLE', 'Subyek:');
+define('TEXT_NEWSLETTER_CONTENT', 'Hanya-Teks <br />Isi:');
+define('TEXT_NEWSLETTER_CONTENT_HTML', 'Teks-Kaya <br />Isi:');
+
+define('TEXT_NEWSLETTER_DATE_ADDED', 'Tanggal Ditambahkan:');
+define('TEXT_NEWSLETTER_DATE_SENT', 'Tanggal Dikirim:');
+
+define('TEXT_INFO_DELETE_INTRO', 'Anda yakin ingin menghapus buletin ini?');
+
+define('TEXT_PLEASE_SELECT_AUDIENCE','Silakan pilih audiens untuk pengiriman buletin ini: ');
+define('TEXT_PLEASE_WAIT', 'Silakan tunggu .. mengirim email ..<br /><br />Tolong jangan mengganggu proses ini!');
+define('TEXT_FINISHED_SENDING_EMAILS', 'Selesai mengirim email!');
+
+define('ERROR_NEWSLETTER_TITLE', 'Kesalahan: Judul Buletin diperlukan');
+define('ERROR_NEWSLETTER_MODULE', 'Kesalahan: Modul Buletin diperlukan');
+?>
