f2aa5841a0205a483b7c8f93ad382923d761b1cc
Hangat Perkasa <ihangat2000@gmail.com>
2019-07-29 23:13:27 +0700

    Add files via upload

---
+++ zc156c-id-v101/includes/languages/indonesia/modules/order_total/ot_coupon.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: DrByte 2019 May 26 Modified in v1.5.6b $
+ */
+
+  define('MODULE_ORDER_TOTAL_COUPON_TITLE', 'Kupon Diskon');
+  define('MODULE_ORDER_TOTAL_COUPON_HEADER', TEXT_GV_NAMES . '/Kupon Diskon');
+  define('MODULE_ORDER_TOTAL_COUPON_DESCRIPTION', 'Kupon Diskon');
+  define('MODULE_ORDER_TOTAL_COUPON_TEXT_ENTER_CODE', TEXT_GV_REDEEM);
+  define('SHIPPING_NOT_INCLUDED', ' [Tidak termasuk pengiriman]');
+  define('TAX_NOT_INCLUDED', ' [Tidak termasuk pajak]');
+  define('IMAGE_REDEEM_VOUCHER', 'Penebusan Voucher');
+  define('MODULE_ORDER_TOTAL_COUPON_REDEEM_INSTRUCTIONS', '<p>Silakan ketikkan kode kupon Anda ke dalam kotak di sebelah Kode Penebusan. Kupon Anda akan diberlakukan terhadap total keseluruhan dan direfleksikan dalam Troli Anda setelah Anda mengklik lanjutkan.</p><p>CATATAN: Anda hanya dapat menggunakan satu buah kupon untuk setiap pesanan.</p>');
+  define('MODULE_ORDER_TOTAL_COUPON_TEXT_CURRENT_CODE', 'Kode Penebusan Anda Saat Ini: ');
+  define('TEXT_COMMAND_TO_DELETE_CURRENT_COUPON_FROM_ORDER', 'CABUT');
+  define('MODULE_ORDER_TOTAL_COUPON_REMOVE_INSTRUCTIONS', '<p>Untuk menghapus sebuah Kupon Diskon dari pesanan ini ketik REMOVE dan tekan Enter atau Return</p>');
+  define('TEXT_REMOVE_REDEEM_COUPON', 'Kupon Diskon Dihapus Sesuai Permintaan!');
+  define('MODULE_ORDER_TOTAL_COUPON_INCLUDE_ERROR', ' Pengaturan Termasuk pajak = true, sebaiknya hanya dilakukan apabila recalculate = None');  
---
+++ zc156c-id-v101/includes/languages/indonesia/modules/order_total/ot_gv.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2007 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: ot_gv.php 6099 2007-04-01 10:22:42Z wilt $
+ */
+
+  define('MODULE_ORDER_TOTAL_GV_TITLE', TEXT_GV_NAMES);
+  define('MODULE_ORDER_TOTAL_GV_HEADER', TEXT_GV_NAMES . '/Kupon Diskon');
+  define('MODULE_ORDER_TOTAL_GV_DESCRIPTION', TEXT_GV_NAMES);
+  define('SHIPPING_NOT_INCLUDED', ' [Tidak termasuk pengiriman]');
+  define('TAX_NOT_INCLUDED', ' [Tidak termasuk pajak]');
+  define('MODULE_ORDER_TOTAL_GV_USER_PROMPT', 'Terapkan Jumlah: ');
+  define('MODULE_ORDER_TOTAL_GV_TEXT_ENTER_CODE', TEXT_GV_REDEEM);
+  define('TEXT_INVALID_REDEEM_AMOUNT', 'Nampaknya jumlah yang Anda coba untuk terapkan dan neraca Kupon Hadiah Anda tidak sama. Silakan coba sekali lagi.');
+  define('MODULE_ORDER_TOTAL_GV_USER_BALANCE', 'Neraca tersedia: ');
+  define('MODULE_ORDER_TOTAL_GV_REDEEM_INSTRUCTIONS', '<p>Untuk menggunakan dana Kupon Hadiah yang telah ada dalam akun Anda, ketikkan jumlah yang Anda inginkan pada kotak \'Terapkan Jumlah\'. Anda akan perlu untuk memilih sebuah cara pembayaran, kemudian klik tombol lanjutkan untuk menerapkan dana ke Troli Anda.</p><p>Jika Anda menebus sebuah Kupon Hadiah <em>baru</em> maka Anda harus mengetik jumlahnya ke dalam kotak di sebelah Kode Penebusan. Jumlah yang ditebus akan ditambahkan ke dalam akun Anda ketika Anda mengklik tombol lanjutkan.</p>');
+  define('MODULE_ORDER_TOTAL_GV_INCLUDE_ERROR', ' Pengaturan termasuk pajak = true, sebaiknya hanya dilakukan apabila recalculate = None');
+?>
