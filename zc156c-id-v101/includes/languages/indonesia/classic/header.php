7917432223f529f48ded2796b897b65a02df795e
Hangat Perkasa <ihangat2000@gmail.com>
2019-07-29 22:56:20 +0700

    Add files via upload

--- zc156c-id-v101/includes/languages/indonesia/classic/header.php
+++ zc156c-id-v101/includes/languages/indonesia/classic/header.php
@@ -1,1 +1,30 @@
+<?php
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: header.php 2848 2006-01-14 09:47:08Z wilt $
+ */
 
+// header text in includes/header.php
+define('HEADER_TITLE_CREATE_ACCOUNT', 'Buat Akun');
+define('HEADER_TITLE_MY_ACCOUNT', 'Akun Saya');
+define('HEADER_TITLE_CART_CONTENTS', 'Troli');
+define('HEADER_TITLE_CHECKOUT', 'Selesai Berbelanja');
+define('HEADER_TITLE_TOP', 'Atas');
+define('HEADER_TITLE_CATALOG', 'Halaman Depan');
+define('HEADER_TITLE_LOGOFF', 'Keluar');
+define('HEADER_TITLE_LOGIN', 'Masuk');
+
+// added defines for header alt and text
+define('HEADER_ALT_TEXT', 'Diberdayakan oleh Zen Cart :: The Art of E-Commerce [tautan halaman depan]');
+define('HEADER_SALES_TEXT', '<h1>Pesan Promosi Anda Disini</h1>');
+define('HEADER_LOGO_WIDTH', '200px');
+define('HEADER_LOGO_HEIGHT', '70px');
+define('HEADER_LOGO_IMAGE', 'logo.gif');
+
+// header Search Button/Box Search Button
+define('HEADER_SEARCH_BUTTON','Cari');
+define('HEADER_SEARCH_DEFAULT_TEXT','Kata Sandi');
+define('SEARCH_DEFAULT_TEXT', 'Cari disini');
